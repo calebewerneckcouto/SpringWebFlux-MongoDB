8e6426a70bdb9a1f634b941238c18fd2128a3ff2
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-03 19:49:08 -0300

    atualizando usuario

--- src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,14 +33,16 @@ public class UserController {
 	public Mono<ResponseEntity<UserDTO>> findById(@PathVariable String id) {
 		return service.findById(id).map(userDto -> ResponseEntity.ok().body(userDto));
 	}
-	
-	
+
 	@PostMapping
-	public Mono<ResponseEntity<UserDTO>> insert(@RequestBody UserDTO dto,UriComponentsBuilder builder) {
-		return service.insert(dto)
-				.map(newUser -> ResponseEntity
-						.created(builder.path("/users/{id}").buildAndExpand(newUser.getId()).toUri()).body(newUser));
+	public Mono<ResponseEntity<UserDTO>> insert(@RequestBody UserDTO dto, UriComponentsBuilder builder) {
+		return service.insert(dto).map(newUser -> ResponseEntity
+				.created(builder.path("/users/{id}").buildAndExpand(newUser.getId()).toUri()).body(newUser));
 	}
-	
+
+	@PutMapping(value = "/{id}")
+	public Mono<ResponseEntity<UserDTO>> update(@PathVariable String id, @RequestBody() UserDTO dto) {
+		return service.update(id, dto).map(userUpdated -> ResponseEntity.ok().body(userUpdated));
+	}
 
 }
