a929edddb83537ab94bff2825c85577507a277cc
calebewerneckcouto <calebewerneck@hotmail.com>
2025-01-03 17:11:45 -0300

    Buscar todos os usuarios e usuario por id

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -3,63 +3,28 @@ package com.devsuperior.workshopmongo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repositories.UserRepository;
+import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repository;
+
+	public Flux<UserDTO> findAll() {
+		return repository.findAll().map(user -> new UserDTO(user));
+	}
 	
 	
-	/*
-
-	@Transactional(readOnly = true)
-	public List<UserDTO> findAll() {
-		List<UserDTO> result = repository.findAll().stream().map(x -> new UserDTO(x)).toList();
-		return result;
+	public Mono<UserDTO> findById(String id) {
+		return repository.findById(id)
+				.map(user -> new UserDTO(user))
+				.switchIfEmpty(Mono.error(new ResourceNotFoundException("Id não encontrado"))); // retorna erro caso nao tenha usuario
 	}
 
-	@Transactional(readOnly = true)
-	public UserDTO findById(String id) {
-		User user = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new UserDTO(user);
-	}
-
-	@Transactional(readOnly = true)
-	public List<PostDTO> findPosts(String id) {
-		User user = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		List<PostDTO> result = user.getPosts().stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
-
-	@Transactional
-	public UserDTO insert(UserDTO dto) {
-		User entity = new User();
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
-	}
-
-	@Transactional
-	public UserDTO update(String id, UserDTO dto) {
-		User entity = repository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new UserDTO(entity);
-	}
-
-	@Transactional
-	public void delete(String id) {
-		User entity = repository.findById(id)
-				.orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		repository.delete(entity);
-	}
-
-	private void copyDtoToEntity(UserDTO dto, User entity) {
-		entity.setName(dto.getName());
-		entity.setEmail(dto.getEmail());
-	}
-	*/
 }
